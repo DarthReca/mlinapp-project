a2cef6373db937da68bbca93b8e03ebce78e2a9a
Daniele R.C <darthreca@gmail.com>
2022-06-06 16:44:15 +0200

    Attgan log images and metrics

--- lit_data_module.py
+++ lit_data_module.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+import torch
 from torch.utils.data import DataLoader, Subset
 from torchvision.datasets import CelebA
 from pytorch_lightning import LightningDataModule
@@ -8,7 +9,9 @@ import numpy as np
 
 
 class CelebADataModule(LightningDataModule):
-    def __init__(self, batch_size: int, indices_file: Optional[str] = None):
+    def __init__(
+        self, batch_size: int, num_workers: int = 4, indices_file: Optional[str] = None
+    ):
         super().__init__()
         self.transform = T.Compose([T.CenterCrop(size=128), T.ToTensor()])
         self.val_dataset = None
@@ -16,33 +19,48 @@ class CelebADataModule(LightningDataModule):
         self.test_dataset = None
         self.batch_size = batch_size
         self.indices = np.load(indices_file) if indices_file else None
+        self.num_workers = num_workers
 
     def prepare_data(self) -> None:
         CelebA(root="data", split="all", download=True)
 
     def setup(self, stage: Optional[str] = None) -> None:
-        # Filter
-        if self.indices is not None:
-            all_dataset = CelebA("data", split="all", transform=self.transform)
-            train_lenght = len(self.indices) * 2 // 3
-        # Make datasets
         if stage == "fit" or stage is None:
             if self.indices is not None:
-                self.train_dataset = Subset(all_dataset, self.indices[:train_lenght])
+                self.train_dataset = Subset(
+                    CelebA("data", split="all", transform=self.transform), self.indices
+                )
             else:
                 self.train_dataset = CelebA(
                     "data", split="train", transform=self.transform
                 )
+            self.val_dataset = Subset(
+                CelebA("data", split="valid", transform=self.transform),
+                torch.arange(10),
+            )
         if stage == "test" or stage is None:
-            if self.indices is not None:
-                self.test_dataset = Subset(all_dataset, self.indices[train_lenght:])
-            else:
-                self.test_dataset = CelebA(
-                    "data", split="test", transform=self.transform
-                )
+            self.test_dataset = CelebA("data", split="test", transform=self.transform)
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+        return DataLoader(
+            self.train_dataset,
+            num_workers=self.num_workers,
+            batch_size=self.batch_size,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_dataset,
+            num_workers=self.num_workers,
+            batch_size=self.batch_size,
+            shuffle=False,
+        )
 
     def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False)
+        return DataLoader(
+            self.test_dataset,
+            num_workers=self.num_workers,
+            batch_size=self.batch_size,
+            shuffle=False,
+        )
