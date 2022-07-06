91801e5e78293715028f00764dc14de5ca587327
ozerodb <50593663+ozerodb@users.noreply.github.com>
2022-07-06 07:38:14 +0200

    added script to extract weights from checkpoint

---
+++ weights/extract_from_checkpoint.py
@@ -0,0 +1,32 @@
+import torch
+import re
+import argparse
+
+def extract_from_checkpoint(input_checkpoint_path, output_weights_path):
+    checkpoint = torch.load(input_checkpoint_path)
+    all_weights = checkpoint['state_dict']
+    weights_without_fid = {k:all_weights[k] for k in all_weights.keys() if not re.match('fid.*', k)}
+    torch.save(weights_without_fid, output_weights_path)
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--checkpoint_path",
+        type=str,
+        required=True,
+        help="the path of the checkpoint from which the weights will be extracted",
+    )
+    parser.add_argument(
+        "--out_path",
+        type=str,
+        required=True,
+        help="the output path of the extracted weights",
+    )
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    extract_from_checkpoint(args.checkpoint_path, args.out_path)
+
+if __name__=="__main__":
+    main()
