aeeed213b35cefceed664bbbec7078b3ed6ecb14
Daniele R.C <darthreca@gmail.com>
2022-07-07 12:44:47 +0200

    Updated sbatchs

--- sbatch/glasses_freeze_4.sh
+++ sbatch/glasses_freeze_4.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=RCGAN
 
-#SBATCH --time=00:40:00
+#SBATCH --time=00:45:00
 
 #SBATCH --mail-user=daniele.regecambrin@studenti.polito.it
 #SBATCH --mail-type=ALL
@@ -23,4 +23,4 @@ module load nvidia/cudasdk/11.6
 source ~/.bashrc
 conda activate mlinapp
 
-python train.py --training_approach="specific" --epochs=30 --batch_size=128 --num_workers=4 --indices_path="data/eyeglasses_only.npy" --experiment_name="trying eyeglasses only dg3" --target_attr="Eyeglasses" --dg_ratio=3 --upload_weights --max_time "00:00:32:00" --freeze_layers 4
+python train.py --training_approach="specific" --epochs=30 --batch_size=128 --num_workers=4 --indices_path="data/eyeglasses_only.npy" --experiment_name="trying eyeglasses only dg3" --target_attr="Eyeglasses" --dg_ratio=5 --upload_weights --max_time "00:00:32:00" --freeze_layers 4
