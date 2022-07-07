13754fdea1afafe21a8d0e9ced5de1824cb6306c
Daniele R.C <darthreca@gmail.com>
2022-07-07 17:42:56 +0200

    updated sbatch

--- sbatch/freeze_4.sh
+++ sbatch/freeze_4.sh
@@ -23,4 +23,4 @@ module load nvidia/cudasdk/11.6
 source ~/.bashrc
 conda activate mlinapp
 
-python train.py --training_approach="specific" --epochs=30 --batch_size=128 --num_workers=4 --indices_path="data/chosen_indices_eyeglasses_smaller.npy" --experiment_name="dg3 freeze4" --target_attr="Eyeglasses" --dg_ratio=3 --upload_weights --max_time "00:01:30:00" --freeze_layers 2 --mode wgan
+python train.py --training_approach="specific" --epochs=30 --batch_size=128 --num_workers=4 --indices_path="data/chosen_indices_eyeglasses_smaller.npy" --experiment_name="dg3 shortcut 4" --target_attr="Eyeglasses" --dg_ratio=3 --upload_weights --max_time "00:01:30:00" --mode wgan --shortcut_layers 4
